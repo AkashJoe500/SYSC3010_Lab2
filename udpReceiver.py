9a6caa811e78d1adf76188eb824dcd1cb14c537b
AkashJoe500 <47267360+AkashJoe500@users.noreply.github.com>
2019-09-20 22:28:05 -0400

    Initial commit

---
+++ udpReceiver.py
@@ -0,0 +1,32 @@
+# Source: https://pymotw.com/2/socket/udp.html
+
+import socket, sys, time
+
+textport = sys.argv[1]
+host = sys.argv[2]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = int(textport)
+server_address = ('localhost', port)
+s.bind(server_address)
+
+while True:
+
+    print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+    buf, address = s.recvfrom(port)
+    
+    if not len(buf):
+        break
+    print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
+    print("ACK: %s" % buf)
+
+
+    print ("Enter data to reply: ENTER to quit")
+    datareceive = sys.stdin.readline().strip()
+
+        #s.sendall(data.encode('utf-8'))
+    #Sending response
+    print("Sending %s" % datareceive)
+    s.sendto(datareceive.encode('utf-8'), server_address)
+        
+s.shutdown(1)
